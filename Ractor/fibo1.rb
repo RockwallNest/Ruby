1e81f6560b5f21be6ad8eb82a204fa01f85c5bef
Akitsu Tsujimoto <43626780+RockwallNest@users.noreply.github.com>
2021-01-11 19:59:24 +0900

    Create fibo1.rb
    Ractorを処理機構に用いたFibonacci数列を作成。

---
+++ Ractor/fibo1.rb
@@ -0,0 +1,51 @@
+require 'benchmark'
+
+class Fibo 
+  N = 10000
+
+  # channel
+  pipe = Ractor.new do 
+    loop do 
+      Ractor.yield Ractor.recv
+    end
+  end
+
+  # routine1
+  $a, $b = 0, 1
+  (1..N).each do 
+    pipe << $a
+    $a, $b = $b, $a+$b
+  end
+
+  # routine2
+  workers = Ractor.new pipe do |pipe|
+    while N
+      n = pipe.take
+      Ractor.yield n
+    end
+  end
+
+  # routine3
+  *sequence = (1..N).map {|i|
+    r, n = Ractor.select(*workers)
+    [i, n]
+  }.sort_by{|i, n| n}
+
+  # p *sequence
+
+end
+
+Benchmark.bm do |x|
+  x.report('seq') { Fibo }
+end
+
+# Fibo
+
+# Ractor並列処理の図解です。 私見ですが...
+#
+#  routine1                           routine2              routine3
+# ----------------                   ----------------      ------------------ 
+# | fibo routine | ---> channel ---> | take routine | ---> | select routine | ---> output
+# ----------------                   ----------------      ------------------
+#
+# 通常のFibo関数の約1140倍の処理速度です｡
