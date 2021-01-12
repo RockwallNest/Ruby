4d5b67a2aadb48c20b641e73496bfef3e425e65b
Akitsu Tsujimoto <43626780+RockwallNest@users.noreply.github.com>
2021-01-12 12:48:04 +0900

    Update fibo2.rb
    while 文をeach文に変えた。

--- Ractor/fibo2.rb
+++ Ractor/fibo2.rb
@@ -5,13 +5,15 @@ N = 10000
 def fib 
   i = 0
   a, b = 0, 1
-  while i < N
+  (1..N).each {
     i += 1
     a, b = b, a+b
-    # p [i, a]
-  end
+    # p "[#{i}, #{a}]"
+  }
 end
 
 Benchmark.bm do |x|
   x.report('seq:') { fib }
 end
+
+#fibo
