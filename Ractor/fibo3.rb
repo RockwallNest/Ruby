a013c80d78cf6f8f921a027e7928a902f8008f09
Akitsu Tsujimoto <43626780+RockwallNest@users.noreply.github.com>
2021-01-17 04:41:29 +0900

    Create fibo3.rb
    クラスメソッドを用いたFibonacci数列を吐き出すコードを作成した。

---
+++ Ractor/fibo3.rb
@@ -0,0 +1,55 @@
+require 'benchmark'
+
+class Fibo
+  # initializer 
+  # pipe, m項を初期化した。
+  def initialize(m)
+    @n = m
+    @pipe = Ractor.new do 
+      loop do 
+        Ractor.yield Ractor.recv
+      end
+    end
+  end
+
+  def fib 
+    # Fibonacci calculating routine
+    @@a, @@b = 0, 1
+    (1..@n).each do 
+      @@a, @@b = @@b, @@a+@@b
+      @pipe << @@a
+    end
+  
+    # Fibonacci generator: Fibonacci数列を生成 
+    gen =  Ractor.new @pipe do |pipe|
+      (1..@n).each do 
+        x = pipe.take 
+        Ractor.yield x
+      end
+    end
+
+    # Fibonacci extractor: Fibonacci数列を抽出
+    # Ractor objectを捨てる。
+    extr = (1..@n).map do |i|
+      r, x = Ractor.select(*gen)
+      [i, x]
+    end
+
+  end
+
+end
+
+# N などの定数を利用するよりも遅くなったけど... 一応できた。
+Benchmark.bm do |x|
+  x.report('seq') {
+    fibo = Fibo.new(10000)
+    fibo.fib
+  }
+end
+
+"""  
+fibo = Fibo.new(10000)
+(fibo.fib).each do |ary|
+  p ary
+end
+"""
