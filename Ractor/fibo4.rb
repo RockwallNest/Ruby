be76ea45d39635f22dc60e12b698939af1694e15
Akitsu Tsujimoto <43626780+RockwallNest@users.noreply.github.com>
2021-01-21 20:32:56 +0900

    Create fibo4.rb
    fib(n)関数を作ってのFibonacci数列掃き出しコードを作成。
    benckmarkは意外に速かった。実際は遅い。

---
+++ Ractor/fibo4.rb
@@ -0,0 +1,52 @@
+require 'benchmark'
+
+class Fibo 
+  def self.fib(n)  
+     a, b = 0, 1
+    if n < 2 
+      1
+    else
+      (1..n).each do
+        a, b = b, a + b
+      end
+      a
+    end
+  end
+
+ pipe = Ractor.new do 
+   loop do 
+     Ractor.yield Ractor.recv 
+   end
+ end
+
+ RN = 10000 
+
+ (1..RN).each do |i|
+   a = fib(i)
+   pipe << a
+ end
+
+ gen = Ractor.new pipe do |pipe|
+   (1..RN).each do
+     x = pipe.take  
+     Ractor.yield x
+   end
+ end 
+
+ extr = (1..RN).each do |i|
+   r, x = Ractor.select(*gen)
+   [i, x] 
+   # [i, x]の代わりに実行すると表示
+   # p [i, x] 
+ end
+
+end 
+
+Benchmark.bm do |x|
+  x.report('seq') {
+    Fibo
+  }
+end
+
+# 表示用
+# Fibo 
